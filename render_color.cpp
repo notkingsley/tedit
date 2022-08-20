95a84ffcfd8b70f8f53096f950ce3deae5959c33
notkingsley <103395094+notkingsley@users.noreply.github.com>
2022-08-20 15:49:19 +0100

    added full-scale syntax coloring for c, c++ and python

---
+++ render_color.cpp
@@ -0,0 +1,403 @@
+#include "tedit.hpp"
+
+static const char* BLUE = "\u001b[38;5;21m";
+static const char* PURPLE = "\u001b[38;5;93m";
+static const char* GREY = "\u001b[38;5;240m";
+static const char* BROWN = "\u001b[38;5;136m";
+static const char* RED = "\u001b[38;5;160m";
+static const char* YELLOW = "\u001b[38;5;11m";
+static const char* PINK = "\u001b[38;5;200m";
+static const char* IDENTIFIER_COLOR = "\u001b[38;5;44m";
+static const char* FUNCTION_COLOR = "\u001b[38;5;110m";
+static const char* RESET = "\u001b[0m";
+
+std::map<std::string, std::string> colors_cpp{
+	{"alignas", BLUE},
+	{"alignof", BLUE},
+	{"asm", BLUE},
+	{"auto", BLUE},
+	{"bool", BLUE},
+	{"char", BLUE},
+	{"class", BLUE},
+	{"concept", BLUE},
+	{"const", BLUE},
+	{"constexpr", BLUE},
+	{"const_cast", BLUE},
+	{"decltype", BLUE},
+	{"double", BLUE},
+	{"dynamic_cast", BLUE},
+	{"enum", BLUE},
+	{"explicit", BLUE},
+	{"export", BLUE},
+	{"extern", BLUE},
+	{"false", BLUE},
+	{"float", BLUE},
+	{"friend", BLUE},
+	{"inline", BLUE},
+	{"int", BLUE},
+	{"long", BLUE},
+	{"mutable", BLUE},
+	{"namespace", BLUE},
+	{"noexcept", BLUE},
+	{"nullptr", BLUE},
+	{"private", BLUE},
+	{"protected", BLUE},
+	{"public", BLUE},
+	{"register", BLUE},
+	{"reinterpret_cast", BLUE},
+	{"requires", BLUE},
+	{"short", BLUE},
+	{"signed", BLUE},
+	{"sizeof", BLUE},
+	{"static", BLUE},
+	{"static_assert", BLUE},
+	{"static_cast", BLUE},
+	{"struct", BLUE},
+	{"synchronized", BLUE},
+	{"template", BLUE},
+	{"this", BLUE},
+	{"thread_local", BLUE},
+	{"typedef", BLUE},
+	{"typeid", BLUE},
+	{"typename", BLUE},
+	{"union", BLUE},
+	{"unsigned", BLUE},
+	{"virtual", BLUE},
+	{"void", BLUE},
+	{"volatile", BLUE},
+	{"wchar_t", BLUE},
+
+
+	{"break", PINK},
+	{"case", PINK},
+	{"catch", PINK},
+	{"continue", PINK},
+	{"default", PINK},
+	{"delete", PINK},
+	{"do", PINK},
+	{"else", PINK},
+	{"for", PINK},
+	{"goto", PINK},
+	{"if", PINK},
+	{"new", PINK},
+	{"operator", PINK},
+	{"return", PINK},
+	{"switch", PINK},
+	{"throw", PINK},
+	{"try", PINK},
+	{"using", PINK},
+	{"while", PINK},
+};
+
+
+std::map<std::string, std::string> colors_c{
+	{"auto", BLUE},
+	{"char", BLUE},
+	{"const", BLUE},
+	{"double", BLUE},
+	{"enum", BLUE},
+	{"extern", BLUE},
+	{"float", BLUE},
+	{"inline", BLUE},
+	{"int", BLUE},
+	{"long", BLUE},
+	{"register", BLUE},
+	{"restrict", BLUE},
+	{"short", BLUE},
+	{"signed", BLUE},
+	{"sizeof", BLUE},
+	{"static", BLUE},
+	{"struct", BLUE},
+	{"typedef", BLUE},
+	{"union", BLUE},
+	{"unsigned", BLUE},
+	{"void", BLUE},
+	{"volatile", BLUE},
+
+
+	{"break", PINK},
+	{"case", PINK},
+	{"continue", PINK},
+	{"default", PINK},
+	{"do", PINK},
+	{"else", PINK},
+	{"for", PINK},
+	{"goto", PINK},
+	{"if", PINK},
+	{"return", PINK},
+	{"switch", PINK},
+	{"while", PINK},
+};
+
+
+std::map<std::string, std::string> colors_python{
+	{"False", BLUE},
+	{"None", BLUE},
+	{"True", BLUE},
+	{"and", BLUE},
+	{"class", BLUE},
+	{"def", BLUE},
+	{"is", BLUE},
+	{"lambda", BLUE},
+	{"nonlocal", BLUE},
+	{"not", BLUE},
+	{"or", BLUE},
+
+
+	{"as", PINK},
+	{"assert", PINK},
+	{"async", PINK},
+	{"await", PINK},
+	{"break", PINK},
+	{"else", PINK},
+	{"elif", PINK},
+	{"else", PINK},
+	{"except", PINK},
+	{"finally", PINK},
+	{"for", PINK},
+	{"from", PINK},
+	{"global", PINK},
+	{"if", PINK},
+	{"import", PINK},
+	{"in", PINK},
+	{"pass", PINK},
+	{"raise", PINK},
+	{"return", PINK},
+	{"try", PINK},
+	{"while", PINK},
+	{"with", PINK},
+	{"yield", PINK},
+};
+
+std::map<std::string, std::string> colors_default { };
+
+std::map<std::string, std::string> colors;
+
+
+static inline bool is_opening_bracket(char c)
+{
+	return c == '[' or c == '{' or c == '(';
+}
+
+static inline bool is_closing_bracket(char c)
+{
+	return c == ']' or c == '}' or c == ')';
+}
+
+static inline bool is_bracket(char c)
+{
+	return is_opening_bracket(c) or is_closing_bracket(c);
+}
+
+static inline bool is_quote(char c)
+{
+	return c == '\'' or c == '"';
+}
+
+static inline bool is_identifier(char  c)
+{
+	return isalnum(c) or c == '_';
+}
+
+static inline bool is_processable_cpp(char c)
+{
+	return is_identifier(c) or is_quote(c) or is_bracket(c) or c == '/';
+}
+
+static inline bool is_processable_py(char c)
+{
+	return is_identifier(c) or is_quote(c) or is_bracket(c) or c == '#';
+}
+
+void begin_render_default(Line* lp)
+{
+	std::cout << *lp;
+}
+
+void begin_render_cpp(Line* lp)
+{
+	if(lp->length() >= 8 and lp->substr(0, 8) == "#include"){
+		std::cout << PINK << lp->substr(0, 8) 
+			<< BROWN << lp->substr(8) << RESET;
+		return;
+	}
+	else if(lp->length() >= 7 and lp->substr(0, 7) == "#define"){
+		std::cout << PINK << lp->substr(0, 7)
+			<< BLUE << lp->substr(7) << RESET;
+		return;
+	}
+	else if(lp->length() >= 6 and lp->substr(0, 6) == "#ifdef"){
+		std::cout << PINK << lp->substr(0, 6)
+			<< BLUE << lp->substr(6) << RESET;
+		return;
+	}
+	else if(lp->length() >= 7 and lp->substr(0, 6) == "#ifndef"){
+		std::cout << PINK << lp->substr(0, 7)
+			<< BLUE << lp->substr(6) << RESET;
+		return;
+	}
+	else if(lp->length() >= 6 and lp->substr(0, 6) == "#undef"){
+		std::cout << PINK << lp->substr(0, 6)
+			<< BLUE << lp->substr(6) << RESET;
+		return;
+	}
+	else if(lp->length() >= 3 and lp->substr(0, 3) == "#if"){
+		std::cout << PINK << lp->substr(0, 3)
+			<< BLUE << lp->substr(3) << RESET;
+		return;
+	}
+	else if(lp->length() >= 5 and lp->substr(0, 5) == "#elif"){
+		std::cout << PINK << lp->substr(0, 5) << RESET;
+		Line temp = *lp;
+		temp.erase(0, 5);
+		begin_render_cpp(&temp);
+		return;
+	}
+	else if(lp->length() >= 5 and lp->substr(0, 5) == "#else"){
+		std::cout << PINK << lp->substr(0, 5) << RESET;
+		Line temp = *lp;
+		temp.erase(0, 5);
+		begin_render_cpp(&temp);
+		return;
+	}
+	else if(lp->length() >= 6 and lp->substr(0, 6) == "#endif"){
+		std::cout << PINK << lp->substr(0, 6) << RESET;
+		Line temp = *lp;
+		temp.erase(0, 6);
+		begin_render_cpp(&temp);
+		return;
+	}
+	else if(lp->length() >= 6 and lp->substr(0, 6) == "#error"){
+		std::cout << PINK << lp->substr(0, 6)
+			<< BROWN << lp->substr(6) << RESET;
+		return;
+	}
+	else if(lp->length() >= 8 and lp->substr(0, 6) == "#warning"){
+		std::cout << PINK << lp->substr(0, 8)
+			<< BROWN << lp->substr(8) << RESET;
+		return;
+	}
+	else if(lp->length() >= 7 and lp->substr(0, 7) == "#pragma"){
+		std::cout << PINK << lp->substr(0, 7)
+			<< BROWN << lp->substr(7) << RESET;
+		return;
+	}
+
+	size_t start = 0;
+	while(start < lp->length()){
+		size_t end = start;
+		while(end < lp->length() and not is_processable_cpp((*lp)[end]))
+			++ end;
+		std::cout << lp->substr(start, end - start);
+		start = end;
+
+		if(end == lp->length())
+			return;
+		
+		if((*lp)[end] == '/' and lp->length() > end + 1 and
+			((*lp)[end + 1] == '/') or (*lp)[end] == '*'){
+			std::cout << GREY << lp->substr(end) << RESET;
+			return;
+		}
+
+		if(is_bracket((*lp)[end])){
+			std::cout << PURPLE << (*lp)[end] << RESET;
+			start = end + 1;
+			continue;
+		}
+
+		if(is_quote((*lp)[end])){
+			start = end++;
+			while(end < lp->length() and (*lp)[end] != (*lp)[start])
+				++end;
+			if(end == lp->length()){
+				std::cout << BROWN << lp->substr(start) << RESET;
+				return;
+			}
+			std::cout << BROWN << lp->substr(start, end - start + 1) << RESET;
+			start = end + 1;
+			continue;
+		}
+
+		if(is_identifier((*lp)[end])){
+			while(end < lp->length() and is_identifier((*lp)[end]))
+				++end;
+			std::string color = colors[lp->substr(start, end - start)];
+			if(color.empty())
+				if(end < lp->length() and (*lp)[end] == '(')
+					std::cout << FUNCTION_COLOR;
+				else
+					std::cout << IDENTIFIER_COLOR;
+			else
+				std::cout << color;
+			std::cout << lp->substr(start, end - start) << RESET;
+			
+			if(end == lp->length())
+				return;
+			start = end;
+			continue;
+		}
+	}
+}
+
+void begin_render_py(Line* lp)
+{
+	size_t start = 0;
+	while(start < lp->length()){
+		size_t end = start;
+		while(end < lp->length() and not is_processable_py((*lp)[end]))
+			++ end;
+		std::cout << lp->substr(start, end - start);
+		start = end;
+
+		if(end == lp->length())
+			return;
+		
+		if((*lp)[end] == '#'){
+			std::cout << GREY << lp->substr(end) << RESET;
+			return;
+		}
+
+		if(is_bracket((*lp)[end])){
+			std::cout << PURPLE << (*lp)[end] << RESET;
+			start = end + 1;
+			continue;
+		}
+
+		if(is_quote((*lp)[end])){
+			if(end <= lp->length() - 3 and lp->substr(end, 3) == "\"\"\""){
+				std::cout << GREY << lp->substr(end) << RESET;
+				return;
+			}
+			start = end++;
+			while(end < lp->length() and (*lp)[end] != (*lp)[start])
+				++end;
+			if(end == lp->length()){
+				std::cout << BROWN << lp->substr(start) << RESET;
+				return;
+			}
+			std::cout << BROWN << lp->substr(start, end - start + 1) << RESET;
+			start = end + 1;
+			continue;
+		}
+
+		if(is_identifier((*lp)[end])){
+			while(end < lp->length() and is_identifier((*lp)[end]))
+				++end;
+			std::string color = colors[lp->substr(start, end - start)];
+			if(color.empty())
+				if(end < lp->length() and (*lp)[end] == '(')
+					std::cout << FUNCTION_COLOR;
+				else
+					std::cout << IDENTIFIER_COLOR;
+			else
+				std::cout << color;
+			std::cout << lp->substr(start, end - start) << RESET;
+			
+			if(end == lp->length())
+				return;
+			start = end;
+			continue;
+		}
+	}
+}
--- renderer.cpp
+++ renderer.cpp
@@ -1,202 +1,26 @@
 #include "tedit.hpp"
 
-static const char* BLUE = "\u001b[38;5;33m";
-static const char* PURPLE = "\u001b[38;5;128m";
+// defined in render_color
 
-static std::map<std::string, std::string> colors_cpp{
-	{"alignas", BLUE},
-	{"alignof", BLUE},
-	{"asm", BLUE},
-	{"auto", BLUE},
-	{"bool", BLUE},
-	{"char", BLUE},
-	{"class", BLUE},
-	{"concept", BLUE},
-	{"const", BLUE},
-	{"constexpr", BLUE},
-	{"const_cast", BLUE},
-	{"decltype", BLUE},
-	{"double", BLUE},
-	{"dynamic_cast", BLUE},
-	{"enum", BLUE},
-	{"explicit", BLUE},
-	{"export", BLUE},
-	{"extern", BLUE},
-	{"false", BLUE},
-	{"float", BLUE},
-	{"friend", BLUE},
-	{"inline", BLUE},
-	{"int", BLUE},
-	{"long", BLUE},
-	{"mutable", BLUE},
-	{"namespace", BLUE},
-	{"noexcept", BLUE},
-	{"nullptr", BLUE},
-	{"private", BLUE},
-	{"protected", BLUE},
-	{"public", BLUE},
-	{"register", BLUE},
-	{"reinterpret_cast", BLUE},
-	{"requires", BLUE},
-	{"short", BLUE},
-	{"signed", BLUE},
-	{"sizeof", BLUE},
-	{"static", BLUE},
-	{"static_assert", BLUE},
-	{"static_cast", BLUE},
-	{"struct", BLUE},
-	{"synchronized", BLUE},
-	{"template", BLUE},
-	{"this", BLUE},
-	{"thread_local", BLUE},
-	{"typedef", BLUE},
-	{"typeid", BLUE},
-	{"typename", BLUE},
-	{"union", BLUE},
-	{"unsigned", BLUE},
-	{"virtual", BLUE},
-	{"void", BLUE},
-	{"volatile", BLUE},
-	{"wchar_t", BLUE},
+extern std::map<std::string, std::string> colors;
+
+extern std::map<std::string, std::string> colors_c;
+
+extern std::map<std::string, std::string> colors_cpp;
+
+extern std::map<std::string, std::string> colors_python;
+
+extern std::map<std::string, std::string> colors_default;
+
+void begin_render_cpp(Line* lp);
+
+void begin_render_py(Line* lp);
+
+void begin_render_default(Line* lp);
+
+void (*begin_render)(Line* lp);
 
 
-	{"break", PURPLE},
-	{"case", PURPLE},
-	{"catch", PURPLE},
-	{"continue", PURPLE},
-	{"default", PURPLE},
-	{"delete", PURPLE},
-	{"do", PURPLE},
-	{"else", PURPLE},
-	{"for", PURPLE},
-	{"goto", PURPLE},
-	{"if", PURPLE},
-	{"new", PURPLE},
-	{"operator", PURPLE},
-	{"return", PURPLE},
-	{"switch", PURPLE},
-	{"throw", PURPLE},
-	{"try", PURPLE},
-	{"using", PURPLE},
-	{"while", PURPLE},
-};
-
-
-static std::map<std::string, std::string> colors_c{
-	{"auto", BLUE},
-	{"char", BLUE},
-	{"const", BLUE},
-	{"double", BLUE},
-	{"enum", BLUE},
-	{"extern", BLUE},
-	{"float", BLUE},
-	{"inline", BLUE},
-	{"int", BLUE},
-	{"long", BLUE},
-	{"register", BLUE},
-	{"restrict", BLUE},
-	{"short", BLUE},
-	{"signed", BLUE},
-	{"sizeof", BLUE},
-	{"static", BLUE},
-	{"struct", BLUE},
-	{"typedef", BLUE},
-	{"union", BLUE},
-	{"unsigned", BLUE},
-	{"void", BLUE},
-	{"volatile", BLUE},
-
-
-	{"break", PURPLE},
-	{"case", PURPLE},
-	{"continue", PURPLE},
-	{"default", PURPLE},
-	{"do", PURPLE},
-	{"else", PURPLE},
-	{"for", PURPLE},
-	{"goto", PURPLE},
-	{"if", PURPLE},
-	{"return", PURPLE},
-	{"switch", PURPLE},
-	{"while", PURPLE},
-};
-
-
-static std::map<std::string, std::string> colors_python{
-	{"False", BLUE},
-	{"None", BLUE},
-	{"True", BLUE},
-	{"and", BLUE},
-	{"class", BLUE},
-	{"def", BLUE},
-	{"is", BLUE},
-	{"lambda", BLUE},
-	{"nonlocal", BLUE},
-	{"not", BLUE},
-	{"or", BLUE},
-
-
-	{"as", PURPLE},
-	{"assert", PURPLE},
-	{"async", PURPLE},
-	{"await", PURPLE},
-	{"break", PURPLE},
-	{"else", PURPLE},
-	{"elif", PURPLE},
-	{"else", PURPLE},
-	{"except", PURPLE},
-	{"finally", PURPLE},
-	{"for", PURPLE},
-	{"from", PURPLE},
-	{"global", PURPLE},
-	{"if", PURPLE},
-	{"import", PURPLE},
-	{"in", PURPLE},
-	{"pass", PURPLE},
-	{"raise", PURPLE},
-	{"return", PURPLE},
-	{"try", PURPLE},
-	{"while", PURPLE},
-	{"with", PURPLE},
-	{"yield", PURPLE},
-};
-
-static std::map<std::string, std::string> colors_default { };
-
-static std::map<std::string, std::string> colors; 
-
-
-// print a word
-static inline void render_word(std::string word)
-{
-	std::cout << colors[word] << word << "\u001b[0m";
-}
-
-static void render_others(const std::string& word, size_t start);
-
-static void render_alpha(const std::string& word, size_t start)
-{
-	size_t end = start;
-	while(end < word.length() and isalpha(word[end]))
-		++end;
-	render_word(word.substr(start, end - start));
-
-	if(end == word.length())
-		return;
-	render_others(word, end);
-}
-
-static void render_others(const std::string& word, size_t start)
-{
-	size_t end = start;
-	while(end < word.length() and not isalpha(word[end]))
-		++end;
-	std::cout << word.substr(start, end - start);
-
-	if(end == word.length())
-		return;
-	render_alpha(word, end);
-}
 
 static void get_win_size(int* res)
 {
@@ -236,6 +60,7 @@ void Renderer::render_doc()
 
 void Renderer::render_line(Line* lp)
 {
+	// set the stage for rendering the line
 	if(lp->position < (*start)->position)
 		return;
 	if(lp->position > (*start)->position + (row_size - 1))
@@ -244,7 +69,7 @@ void Renderer::render_line(Line* lp)
 		return;
 	move_to_line(lp->position - 1, 0);
 	std::cout << "\u001b[2K";
-	render_alpha(*lp, 0);
+	begin_render(lp);
 	std::cout.flush();
 }
 
@@ -275,17 +100,24 @@ void Renderer::move_to(int y, int x)
 void Renderer::initialize_syntax_coloring(std::string filename)
 {
 	colors = colors_default;
+	begin_render = begin_render_default;
 	size_t last = filename.find_last_of('.');
 	if(last == filename.npos)
 		return;
 	std::string ext = filename.substr(last + 1);
 
-	if(ext == "cpp" or ext == "cp" or ext == "hpp")
+	if(ext == "cpp" or ext == "cp" or ext == "hpp"){
 		colors = colors_cpp;
-	else if(ext == "c" or ext == "h")
+		begin_render = begin_render_cpp;
+	}
+	else if(ext == "c" or ext == "h"){
 		colors = colors_c;
-	else if(ext == "py")
+		begin_render = begin_render_cpp;
+	}
+	else if(ext == "py"){
 		colors = colors_python;
+		begin_render = begin_render_py;
+	}
 }
 
 void Renderer::clean_last_line()
